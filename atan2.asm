f35a9c6ea9f2d04e4560cac04f33e465cea34079
glgraca <30938064+glgraca@users.noreply.github.com>
2019-01-18 11:02:42 -0200

    Update atan2.asm
    Added MOVW macro.

--- atan2.asm
+++ atan2.asm
@@ -8,123 +8,117 @@
    80 .asign  JMP &A1DA
    90 .atn    JMP &A90A
   100 .aunp   JMP &A3B5
-  101 .bcopya JMP &A21E
-  102 .aplusb JMP &A505
-  110 \constants
-  120 .pluspi OPT FNEQUF(PI)
-  130 .minuspi OPT FNEQUF(-PI)
-  140 .minushalfpi OPT FNEQUF(-PI/2)
-  150 .halfpi OPT FNEQUF(PI/2)
-  160 .ATAN2
-  170  \Copy address of Z to &50,&51
-  180  CLC
-  190 LDA &4B4
-  200 ADC #&3
-  210 STA &50
-  220 LDA &4B5
-  230 ADC #&0
-  240 STA &51
-  250 \Copy address of X to &52, &53
-  260 CLC
-  270 LDA &4B0
-  280 ADC #&3
-  290 STA &52
-  300 LDA &4B1
-  310 ADC #&0
-  320 STA &53
-  330 \Copy address of Y to &54, &55
-  340 CLC
-  350 LDA &4B2
-  360 ADC #&3
-  370 STA &54
-  380 LDA &4B3
-  390 ADC #&0
-  400 STA &55
-  410 \Load X
-  420 LDA &52
-  430 STA &4B
-  440 LDA &53
-  450 STA &4C
-  460 JSR aunp
-  470 JSR asign
-  480 STA &60
-  490 \Load Y
-  500 LDA &54
-  510 STA &4B
-  520 LDA &55
-  530 STA &4C
-  540 JSR aunp
-  550 JSR asign
-  560 STA &61
-  570 LDA &60
-  580 BNE arctan \X!=0
-  590 LDY #&0
-  600 LDX #&5
-  610 LDA &61
-  620 BPL dohalfpi \Y>0
-  630.dominushalfpi \Y<0
-  640 LDA minushalfpi,Y
-  650 STA (&50),Y
-  660 INY
-  670 DEX
-  680 BNE dominushalfpi
-  690 RTS
-  700 .dohalfpi \Y>0
-  710 LDA halfpi,Y
-  720 STA (&50),Y
-  730 INY
-  740 DEX
-  750 BNE dohalfpi
-  760 RTS
-  770 .arctan \X!=0
-  780 LDA &52
-  790 STA &4B
-  800 LDA &53
-  810 STA &4C
-  820 JSR aunp \fwa=x
-  830 LDA &54
-  840 STA &4B
-  850 LDA &55
-  860 STA &4C  \point to y
-  870 JSR adiv \fwa=y/x
-  880 JSR atn  \fwa=atn(y/x)
-  890 LDA &60
-  900 BPL setz \x>=0
-  901 JSR bcopya
-  910 LDA &61
-  920 BPL addpi \y>=0
-  930.subpi
-  940 LDA #minuspi MOD &100
-  950 STA &4B
-  960 LDA #minuspi DIV &100
-  970 STA &4C
-  980 JMP doadd
-  990.addpi
- 1000 LDA #pluspi MOD &100
- 1010 STA &4B
- 1020 LDA #pluspi DIV &100
- 1030 STA &4C
- 1040.doadd
- 1041 JSR aunp
- 1050 JSR aplusb
- 1051.setz
- 1060 LDA &50
- 1070 STA &4B
- 1080 LDA &51
- 1090 STA &4C
- 1100 JSR apack
- 1120 RTS:]
- 1130 NEXT pass%
- 1140 X=-1
- 1150 Y=1
- 1160 CALL ATAN2
- 1170 PRINT Z
- 1180 END
- 1190 DEF FNEQUF(Z)
- 1200 I% = &3 + ?&4B4 + &100*?&4B5
- 1210 FOR J% = &1 TO &5
- 1220 ?P% = ?I%
- 1230 P% = P% + &1
- 1240 I% = I% + &1
- 1250 NEXT
- 1260 = pass%
+  110 .bcopya JMP &A21E
+  120 .aplusb JMP &A505
+  130 \constants
+  140 .pluspi OPT FNEQUF(PI)
+  150 .minuspi OPT FNEQUF(-PI)
+  160 .minushalfpi OPT FNEQUF(-PI/2)
+  170 .halfpi OPT FNEQUF(PI/2)
+  180 .ATAN2
+  190  \Copy address of Z to &50,&51
+  200  CLC
+  210 LDA &4B4
+  220 ADC #&3
+  230 STA &50
+  240 LDA &4B5
+  250 ADC #&0
+  260 STA &51
+  270 \Copy address of X to &52, &53
+  280 CLC
+  290 LDA &4B0
+  300 ADC #&3
+  310 STA &52
+  320 LDA &4B1
+  330 ADC #&0
+  340 STA &53
+  350 \Copy address of Y to &54, &55
+  360 CLC
+  370 LDA &4B2
+  380 ADC #&3
+  390 STA &54
+  400 LDA &4B3
+  410 ADC #&0
+  420 STA &55
+  430 \Load X
+  440 OPT FNMOVW(&52,&4B)
+  450 JSR aunp
+  460 JSR asign
+  470 STA &60
+  480 \Load Y
+  490 OPT FNMOVW(&54,&4B)
+  500 JSR aunp
+  510 JSR asign
+  520 STA &61
+  530 LDA &60
+  540 BNE arctan \X!=0
+  550 LDY #&0
+  560 LDX #&5
+  570 LDA &61
+  580 BPL dohalfpi \Y>0
+  590.dominushalfpi \Y<0
+  600 LDA minushalfpi,Y
+  610 STA (&50),Y
+  620 INY
+  630 DEX
+  640 BNE dominushalfpi
+  650 RTS
+  660 .dohalfpi \Y>0
+  670 LDA halfpi,Y
+  680 STA (&50),Y
+  690 INY
+  700 DEX
+  710 BNE dohalfpi
+  720 RTS
+  730 .arctan \X!=0
+  740 OPT FNMOVW(&52,&4B) \point to x
+  750 JSR aunp \fwa=x
+  760 OPT FNMOVW(&54,&4B) \point to y
+  770 JSR adiv \fwa=y/x
+  780 JSR atn  \fwa=atn(y/x)
+  790 LDA &60
+  800 BPL setz \x>=0
+  810 JSR bcopya
+  820 LDA &61
+  830 BPL addpi \y>=0
+  840.subpi
+  850 LDA #minuspi MOD &100
+  860 STA &4B
+  870 LDA #minuspi DIV &100
+  880 STA &4C
+  890 JMP doadd
+  900.addpi
+  910 LDA #pluspi MOD &100
+  920 STA &4B
+  930 LDA #pluspi DIV &100
+  940 STA &4C
+  950.doadd
+  960 JSR aunp
+  970 JSR aplusb
+  980.setz
+  990 OPT FNMOVW(&50,&4B)
+ 1000 JSR apack
+ 1010 RTS:]
+ 1020 NEXT pass%
+ 1030 X=-1
+ 1040 Y=1
+ 1050 CALL ATAN2
+ 1060 PRINT Z
+ 1070 END
+ 1080 DEF FNEQUF(Z)
+ 1090 I% = &3 + ?&4B4 + &100*?&4B5
+ 1100 FOR J% = &1 TO &5
+ 1110 ?P% = ?I%
+ 1120 P% = P% + &1
+ 1130 I% = I% + &1
+ 1140 NEXT
+ 1150 =pass%
+ 1160 DEF FNMOVW(F%,T%)
+ 1170 [
+ 1180 OPT pass%
+ 1190 LDA F%
+ 1200 STA T%
+ 1210 LDA F%+&1
+ 1220 STA T%+&1
+ 1230 ]
+ 1240 =pass%
